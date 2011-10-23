bee4fb70f89a84181f9b11ff6fbdca56fe5d532d
Garrett Patterson <garrett.patterson@gmail.com>
2011-10-23 13:24:14 -0700

    adding initial solution

---
+++ SleepyRoutes/HandleAll.cs
@@ -0,0 +1,88 @@
+﻿using System.Web;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Collections.Specialized;
+
+namespace SleepyRoutes
+{
+    public class HandleAll : IHttpHandler
+    {
+        #region IHttpHandler Members
+
+        public bool IsReusable
+        {
+            get { return false; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            HttpRequest Request = context.Request;
+            HttpResponse Response = context.Response;
+            // This handler is called whenever a file ending 
+            // in .sample is requested. A file with that extension
+            // does not need to exist.
+            //path
+            bool handle = false;
+            string file = "";
+            if (handle)
+            {
+                file = "/data/" + Request.Path.Replace("/", ".").Substring(1);
+                //Response.Write(file);
+                NameValueCollection qs = Request.QueryString;
+                foreach (string k in qs)
+                {
+                    file += "." + k;
+                }
+                if (Request.HttpMethod != "GET")
+                {
+                    file += "." + Request.HttpMethod;
+                }
+
+                file = file + ".json";
+                Response.ContentType = "text/json";
+                Response.AddHeader("Content-Type", "text/plain");
+                Response.AddHeader("content-disposition", "inline");
+                try
+                {
+                    Response.TransmitFile(context.Server.MapPath(file));
+                }
+                catch (System.IO.FileNotFoundException ex)
+                {
+                    //Response.Write(file);
+                    //Response.TransmitFile(context.Server.MapPath("/data/404.json"));
+                    if (context.Server.MapPath("/data/404.json") == null)
+                    {
+                        Response.Write("{\"status\":\"error\",\"data\":\"SleepyRoutes, no file found for: " + file + "\"}");
+                    }
+                    else
+                    {
+                        Response.TransmitFile(context.Server.MapPath("/data/404.json"));
+                    }
+                }
+                catch (System.IO.DirectoryNotFoundException ex)
+                {
+                    //Response.Write(file);
+                    //Response.TransmitFile(context.Server.MapPath("/data/404.json"));
+                    if (context.Server.MapPath("/data/404.json") == null)
+                    {
+                        Response.Write("{\"status\":\"error\",\"data\":\"SleepyRoutes, no file found for: " + file + "\"}");
+                    }
+                    else
+                    {
+                        Response.TransmitFile(context.Server.MapPath("/data/404.json"));
+                    }
+                }
+                Response.Flush();
+                Response.End();
+            }
+            else
+            {
+
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ SleepyRoutes/HandleREST.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections.Specialized;
+
+namespace SleepyRoutes
+{
+    class HandleRest : IHttpModule
+    {
+        #region IHttpModule Members
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest +=
+                new EventHandler(Context_BeginRequest);
+        }
+
+        void Context_BeginRequest(object sender, EventArgs e)
+        {
+            HttpRequest Request = ((HttpApplication)sender).Request;
+            ProcessRequest((HttpApplication)sender);
+
+        }
+
+        #endregion
+
+        public void ProcessRequest(HttpApplication context)
+        {
+            HttpRequest Request = context.Request;
+            HttpResponse Response = context.Response;
+
+            bool handle = !(Regex.Match(Request.RawUrl, @"\w{1,}\.\w{2,4}(\?*)?$").Success);
+            string file = "";
+            if (handle)
+            {
+                file = "/data/" + Request.Path.Replace("/", ".").Substring(1);
+                //Response.Write(file);
+                NameValueCollection qs = Request.QueryString;
+                foreach (string k in qs)
+                {
+                    if (k != "Env")
+                    {
+                        file += "." + k;
+                    }
+                }
+                if (Request.HttpMethod != "GET")
+                {
+                    file += "." + Request.HttpMethod;
+                }
+
+                file = file + ".json";
+                Response.ContentType = "text/json";
+                Response.AddHeader("Content-Type", "text/plain");
+                Response.AddHeader("content-disposition", "inline");
+                try
+                {
+                    Response.TransmitFile(context.Server.MapPath(file));
+                }
+                catch (System.IO.FileNotFoundException ex)
+                {
+                    Response.TransmitFile(context.Server.MapPath("/data/404.json"));
+                }
+                catch (System.IO.DirectoryNotFoundException ex)
+                {
+                    Response.TransmitFile(context.Server.MapPath("/data/404.json"));
+                }
+                Response.Flush();
+                Response.End();
+            }
+
+
+        }
+    }
+}
